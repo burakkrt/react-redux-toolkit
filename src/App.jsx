ce9ae04bdab72194dfe49b49c5c24c2e06a95244
BurakKurt <krtburak@outlook.com>
2023-07-06 15:05:06 +0300

    project created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from 'antd';
+function App() {
+
+    const {Title} = Typography;
+
+  return (
+    <div>
+        <Title level={3} type="success">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Title>
+    </div>
+  )
+}
+
+export default App
