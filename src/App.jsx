d5965272fcc2b4d07e1dfb3f3fe6cd186f889815
Burak <99482906+burakkrt@users.noreply.github.com>
2023-07-07 15:23:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <div>
         <Title level={3} type="success">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Title>
+          <Text type="warning">The under construction</Text>
+          <Text type="secondary">TR : Yapım Aşamasında.</Text>
     </div>
   )
 }
